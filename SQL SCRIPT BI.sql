ea1006b038e2decd27f6f53e1cf485b8d5ea3594
Federico Sepulveda <37560557+heinHertz@users.noreply.github.com>
2020-07-13 21:55:37 -0300

    Update SQL SCRIPT BI.sql

--- SQL SCRIPT BI.sql
+++ SQL SCRIPT BI.sql
@@ -15,50 +15,103 @@ IF OBJECT_ID('clientesVentasPasajes','P') IS NOT NULL
 	DROP PROCEDURE clientesVentasPasajes
 
 go
-Create procedure clientesVentasHabitacion
-AS
-begin
 
-Select c.ID_CLIENTE, c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO,MONTH(com.COMPRA_FECHA)as "Mes",Year(com.COMPRA_FECHA), SUM(h.HABITACION_PRECIO*es.ESTADIA_CANTIDAD_NOCHES) AS "Total vendido por habitaciones" 
-from Los_dateros.cliente c join 
-	LOS_DATEROS.factura f on f.id_cliente = c.ID_CLIENTE and f.id_cliente != 1 join
-	LOS_DATEROS.compra com	on f.id_compra = com.COMPRA_NUMERO and com.ID_COMPRA != 1 join 
-	LOS_DATEROS.estadia es	on es.ID_ESTADIA = com.ID_ESTADIA and es.ID_ESTADIA != 1 join
-	LOS_DATEROS.habitacion h on h.ID_HABITACION = es.id_habitacion and h.ID_HABITACION != 1
-	group by c.ID_CLIENTE,c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO
-	order by 5 desc
-end
-go
+--create procedure clientesVentasHabitacion
+--AS
+--begin
 
-go
-create Procedure ClientesVentasPasajes
-as
-begin 
-SELECT c.ID_CLIENTE, c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO,MonTH(com.COMPRA_FECHA) as "Mes",Year(com.COMPRA_FECHA), SUM(p.PASAJE_COSTO) as "Total vendido por pasajes" 
-from Los_dateros.cliente c join 
-	LOS_DATEROS.factura f on f.id_cliente = c.ID_CLIENTE and f.id_cliente != 1 join
-	LOS_DATEROS.compra com	on f.id_compra = com.COMPRA_NUMERO and com.ID_COMPRA != 1 join
-	Los_Dateros.butacas b on b.id_butaca = com.ID_BUTACA and b.id_butaca != 1 join
-	LOS_DATEROS.pasajes p on p.id_pasaje = b.id_butaca and p.id_pasaje != 1 
-	group by c.ID_CLIENTE, c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO, COM.COMPRA_FECHA
-	order by 4 desc
-end
-go
+--Select c.ID_CLIENTE, c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO,MONTH(com.COMPRA_FECHA)as "Mes",Year(com.COMPRA_FECHA), SUM(h.HABITACION_PRECIO*es.ESTADIA_CANTIDAD_NOCHES) AS "Total vendido por habitaciones" 
+--from Los_dateros.cliente c join 
+--	LOS_DATEROS.factura f on f.id_cliente = c.ID_CLIENTE and f.id_cliente != 1 join
+--	LOS_DATEROS.compra com	on f.compra_Numero = com.COMPRA_NUMERO and com.ID_COMPRA != 1 join 
+--	LOS_DATEROS.estadia es	on es.ID_ESTADIA = com.ID_ESTADIA and es.ID_ESTADIA != 1 join
+--	LOS_DATEROS.habitacion h on h.ID_HABITACION = es.id_habitacion and h.ID_HABITACION != 1
+--	group by c.ID_CLIENTE,c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO
+--	order by 5 desc
+--end
+--go
 
-CREATE TABLE [LOS_DATEROS].[DimVentasAClientes](				
-			[ID_CLIENTE] [bigint] NULL,
-			[CLIENTE_NOMBRE] [nvarchar](255) NULL,
-			[CLIENTE_APELLIDO] [nvarchar](255) NULL,
-			[VENTAS_PASAJES] [int] NULL,
-			[VENTAS_HABITACIONES] [int] NULL,
-			[MES] [int] NULL,
-			[AÑO] [int] NULL
-		) ON [PRIMARY]	
+--go
+--create Procedure ClientesVentasPasajes
+--as
+--begin 
+--SELECT c.ID_CLIENTE, c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO,MonTH(com.COMPRA_FECHA) as "Mes",Year(com.COMPRA_FECHA), SUM(p.PASAJE_COSTO) as "Total vendido por pasajes" 
+--from Los_dateros.cliente c join 
+--	LOS_DATEROS.factura f on f.id_cliente = c.ID_CLIENTE and f.id_cliente != 1 join
+--	LOS_DATEROS.compra com	on f.compra_Numero = com.COMPRA_NUMERO and com.ID_COMPRA != 1 join
+--	Los_Dateros.butacas b on b.id_butaca = com.ID_BUTACA and b.id_butaca != 1 join
+--	LOS_DATEROS.pasajes p on p.id_pasaje = b.id_butaca and p.id_pasaje != 1 
+--	group by c.ID_CLIENTE, c.CLIENTE_NOMBRE,c.CLIENTE_APELLIDO, COM.COMPRA_FECHA
+--	order by 4 desc
+--end
+--go
+
+--IF OBJECT_ID('LOS_DATEROS.ciudad', 'U') IS NOT NULL
+--			DROP TABLE LOS_DATEROS.[DimVentasAClientes]
+
+--CREATE TABLE [LOS_DATEROS].[DimVentasAClientes](				
+--			[ID_CLIENTE] [bigint] NULL,
+--			[CLIENTE_NOMBRE] [nvarchar](255) NULL,
+--			[CLIENTE_APELLIDO] [nvarchar](255) NULL,
+--			[VENTAS_PASAJES] [int] NULL,
+--			[VENTAS_HABITACIONES] [int] NULL,
+--			[MES] [int] NULL,
+--			[AÑO] [int] NULL
+--		) ON [PRIMARY]	
 
 -- lo que quiero hacer es unir los dos procedures en esa "dimension"
 
-exec clientesVentasHabitacion
-exec ClientesVentasPasajes
+	--		exec clientesVentasHabitacion
+	--		exec ClientesVentasPasajes
+
+
+--select DATEDIFF(YEAR,cc.CLIENTE_FECHA_NAC,GETDATE()) from [LOS_DATEROS].cliente cc order by DATEDIFF(YEAR,cc.CLIENTE_FECHA_NAC,GETDATE()) where  cc.CLIENTE_DNI='1138084'
+
+--select * from [LOS_DATEROS].cliente cc 
 
 
 --Select count(1), MONTH(C.COMPRA_FECHA) AS "MES" from LOS_DATEROS.compra C where c.ID_EMPRESA != 1 group by MONTH(C.COMPRA_FECHA) 
+
+IF OBJECT_ID('datosClientes','P') IS NOT NULL
+	DROP PROCEDURE datosClientes
+
+
+IF OBJECT_ID('LOS_DATEROS.Dimension_Cliente', 'U') IS NOT NULL
+			DROP TABLE LOS_DATEROS.[Dimension_Cliente]		
+
+CREATE TABLE [LOS_DATEROS].[Dimension_Cliente](				
+			[ID_CLIENTE] [bigint] not NULL,
+			[CLIENTE_DNI] [decimal](18, 0) NULL,
+			[CLIENTE_NOMBRE] [nvarchar](255) NULL,
+			[CLIENTE_APELLIDO] [nvarchar](255) NULL,
+			[EDAD] TINYINT null,
+			[CLIENTE_TELEFONO] [int] NULL,
+			[CLIENTE_MAIL] [nvarchar](255) NULL
+	) ON [PRIMARY]
+
+
+go
+create Procedure datosClientes
+as
+begin 
+
+
+		insert into [LOS_DATEROS].Dimension_Cliente([ID_CLIENTE] ,[CLIENTE_DNI] ,[CLIENTE_NOMBRE] ,	[CLIENTE_APELLIDO] ,[EDAD] ,[CLIENTE_TELEFONO] ,[CLIENTE_MAIL] ) 
+		 select
+		cc.ID_CLIENTE,
+		cc.CLIENTE_DNI,
+		cc.CLIENTE_NOMBRE,
+		cc.CLIENTE_APELLIDO,
+		(select DATEDIFF(YEAR,cc.CLIENTE_FECHA_NAC,GETDATE()) ),
+		cc.CLIENTE_TELEFONO,
+		cc.CLIENTE_MAIL
+		from [LOS_DATEROS].cliente cc  
+end
+go
+
+
+	exec datosClientes
+
+--		select * from [LOS_DATEROS].Dimension_Cliente
+
+
